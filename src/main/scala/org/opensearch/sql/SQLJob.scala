fd0b37068ad657279d77a285e4c31f32dd17fb84
Peng Huo <penghuo@gmail.com>
2023-04-07 17:58:19 -0700

    support streaming job
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- src/main/scala/org/opensearch/sql/SQLJob.scala
+++ src/main/scala/org/opensearch/sql/SQLJob.scala
@@ -1,22 +1,31 @@
 package org.opensearch.sql
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.{avg, count, window}
+import org.apache.spark.sql.{SparkSession}
 
 object SQLJob {
   def main(args: Array[String]) {
-    // Get the SQL query from the command line arguments
-    val sql = args(0)
+    val spark = SparkSession.builder()
+      .config(
+        "spark.hadoop.hive.metastore.client.factory.class",
+        "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",
+      )
+      .enableHiveSupport()
+      .getOrCreate()
 
-    // Create a SparkSession
-    val spark = SparkSession.builder().appName("SQLJob").getOrCreate()
+    import spark.implicits._  // Import SQL implicits to use $ notation
 
-    // Execute the SQL query
-    val df: Dataset[Row] = spark.sql(sql)
-
-    // Show the results
-    df.show()
-
-    // Stop the SparkSession
-    spark.stop()
+    spark.readStream.table("default.alb_logs_temp")
+      .withWatermark("time", "10 seconds")
+      .groupBy(window($"time", "1 minute"))
+      .agg(count("time").alias("request_count"), avg("request_processing_time").alias("Latency_in_seconds"))
+      .writeStream
+      .queryName("alb_logs_metrics")
+      .format("parquet")
+      .outputMode("append")
+      .option("checkpointLocation", "/tmp/alb_logs_metrics")
+      .option("path", "s3a://flint.dev.penghuo.us-west-2/alb_logs_metrics/")
+      .start()
+      .awaitTermination()
   }
 }
