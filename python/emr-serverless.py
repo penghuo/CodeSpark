d74170be03cbe56b668e5b8b660e9d726e940422
Peng Huo <penghuo@gmail.com>
2023-08-10 08:24:59 -0700

    change the emr-serverless to per session per query
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- python/emr-serverless.py
+++ python/emr-serverless.py
@@ -5,6 +5,8 @@ from boto3 import session
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
 
+applicationId="00fc9qgmi20r560l"
+
 def run_shell_command(cmd):
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
@@ -19,14 +21,14 @@ def run_shell_command(cmd):
 
 def executeQuery():
   # start-calculation
-  r=run_shell_command('aws emr-serverless start-job-run --application-id 00fc67loi6kb7p0l --execution-role-arn arn:aws:iam::483560928347:role/FlintEMRServerlessS3RuntimeRole  --job-driver \'{"sparkSubmit": {"entryPoint": "s3://flint.dev.penghuo.us-west-2/lib/jdk8/sql-job.jar","entryPointArguments":["select 1"],"sparkSubmitParameters":"--class org.opensearch.sql.SQLJob --conf spark.executor.cores=1 --conf spark.executor.memory=2g --conf spark.driver.cores=1 --conf spark.driver.memory=2g"}}\' --profile flintappsec')
+  r=run_shell_command('aws emr-serverless start-job-run --application-id 00fc9qgmi20r560l --execution-role-arn arn:aws:iam::483560928347:role/FlintEMRServerlessS3RuntimeRole  --job-driver \'{"sparkSubmit": {"entryPoint": "s3://flint.dev.penghuo.us-west-2/lib/jdk8/sql-job.jar","entryPointArguments":["select 1"],"sparkSubmitParameters":"--class org.opensearch.sql.SQLJob --conf spark.executor.cores=1 --conf spark.executor.memory=2g --conf spark.driver.cores=1 --conf spark.driver.memory=2g"}}\' --profile flintappsec')
   jr = json.loads(r)
   jobRunId=jr['jobRunId']
   print("start-job-run: " + jobRunId)
 
   # get-calculation-execution
   while True:
-     r=run_shell_command(f'aws emr-serverless get-job-run --application-id 00fc67loi6kb7p0l --job-run-id {jobRunId}')
+     r=run_shell_command(f'aws emr-serverless get-job-run --application-id 00fc9qgmi20r560l --job-run-id {jobRunId}')
      jr = json.loads(r)
      state=jr['jobRun']['state']
      if state == 'SUCCESS':
