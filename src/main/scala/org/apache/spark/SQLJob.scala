f5b56c316460a2d0c0d91b1b51680e22d9671ccd
Peng Huo <penghuo@gmail.com>
2024-10-02 07:48:55 -0700

    concurrent submit
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- src/main/scala/org/apache/spark/SQLJob.scala
+++ src/main/scala/org/apache/spark/SQLJob.scala
@@ -1,78 +1,87 @@
-package org.opensearch.sql
+package org.apache.spark
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
 
+import scala.concurrent.Future
+import scala.util.Try
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object SQLJob extends Logging {
-  // // Define a function to recursively unpersist RDD and its dependencies
-  // def unpersistRecursive(rdd: RDD[_]): Unit = {
-  //   // Unpersist the current RDD
-  //   logInfo(s">> before unpersist")
-  //   rdd.unpersist(true)
-  //   logInfo(s">> after unpersist")
-
-  //   logInfo(s">> before cleanShuffleDependencies")
-  //   rdd.cleanShuffleDependencies(true)
-  //   logInfo(s"<< after cleanShuffleDependencies")
-    
-  //   // Recursively unpersist each dependency RDD
-  //   rdd.dependencies.foreach { dep =>
-  //     val depRDD = dep.rdd
-  //     unpersistRecursive(depRDD)
-  //   }
-  // }
-
   def main(args: Array[String]) {
-    val query =  args(0)
-    val clearShuffle = args(1).toBoolean
+    if (args.length != 4) {
+      logError("Usage: SparkQueryApp <track> <id><iterations> <query>")
+      System.exit(1)
+    }
 
-    // create spark session
+    val track = args(0)
+    val id = args(1)
+    val iterations = Try(args(2).toInt).getOrElse {
+      logError("Invalid number of iterations")
+      System.exit(1)
+      0
+    }
+    val query = args(3)
+        // Initialize Spark session with AWS Glue Data Catalog as metastore
     val spark = SparkSession.builder()
       .config(
         "spark.hadoop.hive.metastore.client.factory.class",
-        "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",
+        "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
       )
       .enableHiveSupport()
       .getOrCreate()
 
-    // Execute each SQL query with a 10-second delay in between
-    logInfo(s"Executing query: $query")
+    // warmup
+    logInfo(s"Executing warmup query: $query")
     val startTime = System.nanoTime()
-    
-    // Execute SQL query
-    var result = spark.sql(query)
-    
-    // Show the result of the query
-    result.show()
+    try {
+      spark.sql(query).show()
+    } catch {
+      case e: Exception =>
+        logError(s"Error executing query: $query", e)
+    }
+    val endTime = System.nanoTime()
+    val duration = (endTime - startTime) / 1e9d // Convert to seconds
+    logInfo(s"Finished warmup in $duration seconds")
 
-    if (clearShuffle) {
-//     logInfo(s">> before cleanShuffleDependencies")
-//     result.rdd.cleanShuffleDependencies(true)
-//     logInfo(s"<< after cleanShuffleDependencies")
 
-      logInfo("Waiting for 1 mins...")
-      Thread.sleep(60.seconds.toMillis)
+    val futures = (1 to iterations).map { i =>
+      Future {
+        logInfo(s"Executing query iteration $i: $query")
+        val startTime = System.nanoTime()
 
-//     logInfo(s">> before unpersist")
-//     result.rdd.unpersist(true)
-//     logInfo(s">> after unpersist")
+        try {
+          spark.sql(query).show()
+        } catch {
+          case e: Exception =>
+            logError(s"Error executing query iteration $i: $query", e)
+        }
 
-      logInfo(s">> Before gc")
-      System.gc()
-      logInfo(s"<< After gc")
+        val endTime = System.nanoTime()
+        val duration = (endTime - startTime) / 1e9d
+        logInfo(s"Finished iteration $i in $duration seconds")
+        duration
+      }
     }
 
-    // Calculate and print the execution time
-    val endTime = System.nanoTime()
-    val duration = (endTime - startTime) / 1e9d
-    println(s"Query executed in $duration seconds")
+    // Wait for all futures to complete and collect results
+    import scala.concurrent.Await
+    import scala.concurrent.duration.Duration
+    val latencies = Await.result(Future.sequence(futures), Duration.Inf)    
 
-    logInfo("Waiting for 5 mins...")
-    Thread.sleep(300.seconds.toMillis)
+    // Calculate latencies: max, p50, and p75
+    val sortedLatencies = latencies.sorted
+    val maxLatency = sortedLatencies.last
+    val p50Latency = sortedLatencies((0.5 * iterations).toInt)
+    val p75Latency = sortedLatencies((0.75 * iterations).toInt)
 
-    // Stop the SparkSession
-    spark.stop()    
+    logInfo(f"$track-$id, max Latency: $maxLatency%.2f seconds")
+    logInfo(f"$track-$id, p50 Latency: $p50Latency%.2f seconds")
+    logInfo(f"$track-$id, p75 Latency: $p75Latency%.2f seconds")
+
+    // Stop the Spark session
+    spark.stop()
+    System.exit(0)
   }
 }
